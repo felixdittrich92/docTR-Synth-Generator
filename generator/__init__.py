de3d75b3cfeb2c04c221629d89bd6dc0e518f39b
felix <felixdittrich92@gmail.com>
2025-08-14 15:57:13 +0200

    Update init with missing version export

--- generator/__init__.py
+++ generator/__init__.py
@@ -1,3 +1,4 @@
 from .dataset_generator import SyntheticDatasetGenerator, GenerationConfig
+from .version import __version__  # noqa: F401
 
 __all__ = ["SyntheticDatasetGenerator", "GenerationConfig"]
